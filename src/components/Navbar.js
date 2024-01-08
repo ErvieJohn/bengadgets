07e76f11c3d2961391bf2e2d846c7e4cc91e0020
ErvieJohn <erviejohn.villareal79@gmail.com>
2024-01-08 14:13:31 +0800

    Added First Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faShoppingBasket } from '@fortawesome/fontawesome-free-solid';
+import logo from '../logo.svg'
+
+function Navbar() {
+  return (
+    <div className='nav-body'>
+        <div className='nav-container'>
+            <img width={100} height={50} src={logo} alt='logo'>
+            </img>
+
+            <div className='nav-item' style={{ width: "35%" }}>
+                <a href='#' className='nav-item-link'>
+                    <h5>Home</h5>
+                </a>
+                <a href='#' className='nav-item-link'>
+                    <h5>Products</h5>
+                </a>
+                <a href='#' className='nav-item-link'>
+                    <h5>About</h5>
+                </a>
+                <a href='#' className='nav-item-link'>
+                    <h5>Contacts</h5>
+                </a>
+            </div>
+
+            <div className='nav-item' style={{ width: "10%" }}>
+                <button style={{ backgroundColor: "transparent", border: "none" }}>
+                    <FontAwesomeIcon icon={faSearch} color='black' size='1x'/>
+                </button>
+                
+                <button style={{ backgroundColor: "transparent", border: "none" }}>
+                    <FontAwesomeIcon icon={faShoppingBasket} color='black' size='1x'/>
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
